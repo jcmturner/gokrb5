dd8259545a48dcc4ca04978737d45bec3779a2b4
Jonathan Turner <jt@jtnet.co.uk>
2017-01-22 18:59:01 +0000

    KDC Req tests

--- messages/KDCReq.go
+++ messages/KDCReq.go
@@ -11,6 +11,13 @@ import (
 	"time"
 )
 
+type marshalKDCReq struct {
+	PVNO    int                  `asn1:"explicit,tag:1"`
+	MsgType int                  `asn1:"explicit,tag:2"`
+	PAData  types.PADataSequence `asn1:"explicit,optional,tag:3"`
+	ReqBody asn1.RawValue        `asn1:"explicit,tag:4"`
+}
+
 type KDCReq struct {
 	PVNO    int            `asn1:"explicit,tag:1"`
 	MsgType int            `asn1:"explicit,tag:2"`
@@ -18,27 +25,8 @@ type KDCReq struct {
 	ReqBody KDCReqBody     `asn1:"explicit,tag:4"`
 }
 
-type marshalKDCReq struct {
-	PVNO    int               `asn1:"explicit,tag:1"`
-	MsgType int               `asn1:"explicit,tag:2"`
-	PAData  []types.PAData    `asn1:"explicit,optional,tag:3"`
-	ReqBody marshalKDCReqBody `asn1:"explicit,tag:4"`
-}
-
-type KDCReqBody struct {
-	KDCOptions        asn1.BitString      `asn1:"explicit,tag:0"`
-	CName             types.PrincipalName `asn1:"explicit,optional,tag:1"`
-	Realm             string              `asn1:"explicit,tag:2"`
-	SName             types.PrincipalName `asn1:"explicit,optional,tag:3"`
-	From              time.Time           `asn1:"explicit,optional,tag:4"`
-	Till              time.Time           `asn1:"explicit,tag:5"`
-	RTime             time.Time           `asn1:"explicit,optional,tag:6"`
-	Nonce             int                 `asn1:"explicit,tag:7"`
-	EType             []int               `asn1:"explicit,tag:8"`
-	Addresses         []types.HostAddress `asn1:"explicit,optional,tag:9"`
-	EncAuthData       types.EncryptedData `asn1:"explicit,optional,tag:10"`
-	AdditionalTickets []types.Ticket      `asn1:"explicit,optional,tag:11"`
-}
+type ASReq KDCReq
+type TGSReq KDCReq
 
 type marshalKDCReqBody struct {
 	KDCOptions  asn1.BitString      `asn1:"explicit,tag:0"`
@@ -56,49 +44,87 @@ type marshalKDCReqBody struct {
 	AdditionalTickets asn1.RawValue `asn1:"explicit,optional,tag:11"`
 }
 
-func UnmarshalASReq(b []byte) (k KDCReq, err error) {
-	k, err = unmarshalKDCReq(b, asnAppTag.ASREQ)
-	if err != nil {
-		return k, err
-	}
-	expectedMsgType := types.KrbDictionary.MsgTypesByName["KRB_AS_REQ"]
-	if k.MsgType != expectedMsgType {
-		return k, fmt.Errorf("Message ID does not indicate a KRB_AS_REQ. Expected: %v; Actual: %v", expectedMsgType, k.MsgType)
-	}
-	return k, nil
+type KDCReqBody struct {
+	KDCOptions        asn1.BitString      `asn1:"explicit,tag:0"`
+	CName             types.PrincipalName `asn1:"explicit,optional,tag:1"`
+	Realm             string              `asn1:"explicit,tag:2"`
+	SName             types.PrincipalName `asn1:"explicit,optional,tag:3"`
+	From              time.Time           `asn1:"explicit,optional,tag:4"`
+	Till              time.Time           `asn1:"explicit,tag:5"`
+	RTime             time.Time           `asn1:"explicit,optional,tag:6"`
+	Nonce             int                 `asn1:"explicit,tag:7"`
+	EType             []int               `asn1:"explicit,tag:8"`
+	Addresses         []types.HostAddress `asn1:"explicit,optional,tag:9"`
+	EncAuthData       types.EncryptedData `asn1:"explicit,optional,tag:10"`
+	AdditionalTickets []types.Ticket      `asn1:"explicit,optional,tag:11"`
 }
 
-func unmarshalKDCReq(b []byte, asnAppTag int) (k KDCReq, err error) {
+func (k *ASReq) Unmarshal(b []byte) error {
 	var m marshalKDCReq
-	//fmt.Fprintf(os.Stderr, "b: %+v\n", b)
-	_, err = asn1.UnmarshalWithParams(b, &m, fmt.Sprintf("application,explicit,tag:%v", asnAppTag))
+	_, err := asn1.UnmarshalWithParams(b, &m, fmt.Sprintf("application,explicit,tag:%v", asnAppTag.ASREQ))
 	if err != nil {
-		err = fmt.Errorf("Error unmarshalling KDC_REQ: %v", err)
-		return
+		return fmt.Errorf("Error unmarshalling KDC_REQ: %v", err)
 	}
-	//fmt.Fprintf(os.Stderr, "req: %+v", m)
-	var reqb = KDCReqBody{
-		KDCOptions:  m.ReqBody.KDCOptions,
-		CName:       m.ReqBody.CName,
-		Realm:       m.ReqBody.Realm,
-		SName:       m.ReqBody.SName,
-		From:        m.ReqBody.From,
-		Till:        m.ReqBody.Till,
-		RTime:       m.ReqBody.RTime,
-		Nonce:       m.ReqBody.Nonce,
-		EType:       m.ReqBody.EType,
-		Addresses:   m.ReqBody.Addresses,
-		EncAuthData: m.ReqBody.EncAuthData,
+	expectedMsgType := types.KrbDictionary.MsgTypesByName["KRB_AS_REQ"]
+	if m.MsgType != expectedMsgType {
+		return fmt.Errorf("Message ID does not indicate a KRB_AS_REQ. Expected: %v; Actual: %v", expectedMsgType, m.MsgType)
+	}
+	var reqb KDCReqBody
+	err = reqb.Unmarshal(m.ReqBody.Bytes)
+	if err != nil {
+		return fmt.Errorf("Error processing KDC_REQ_BODY: %v", err)
 	}
 	k.MsgType = m.MsgType
 	k.PAData = m.PAData
 	k.PVNO = m.PVNO
-	if len(m.ReqBody.AdditionalTickets.Bytes) > 0 {
-		reqb.AdditionalTickets, err = types.UnmarshalSequenceTickets(m.ReqBody.AdditionalTickets)
+	k.ReqBody = reqb
+	return nil
+}
+
+func (k *TGSReq) Unmarshal(b []byte) error {
+	var m marshalKDCReq
+	_, err := asn1.UnmarshalWithParams(b, &m, fmt.Sprintf("application,explicit,tag:%v", asnAppTag.TGSREQ))
+	if err != nil {
+		return fmt.Errorf("Error unmarshalling KDC_REQ: %v", err)
+	}
+	expectedMsgType := types.KrbDictionary.MsgTypesByName["KRB_TGS_REQ"]
+	if m.MsgType != expectedMsgType {
+		return fmt.Errorf("Message ID does not indicate a KRB_TGS_REQ. Expected: %v; Actual: %v", expectedMsgType, m.MsgType)
+	}
+	var reqb KDCReqBody
+	err = reqb.Unmarshal(m.ReqBody.Bytes)
+	if err != nil {
+		return fmt.Errorf("Error processing KDC_REQ_BODY: %v", err)
+	}
+	k.MsgType = m.MsgType
+	k.PAData = m.PAData
+	k.PVNO = m.PVNO
+	k.ReqBody = reqb
+	return nil
+}
+
+func (k *KDCReqBody) Unmarshal(b []byte) error {
+	var m marshalKDCReqBody
+	_, err := asn1.Unmarshal(b, &m)
+	if err != nil {
+		return fmt.Errorf("Error unmarshalling KDC_REQ_BODY: %v", err)
+	}
+	k.KDCOptions = m.KDCOptions
+	k.CName = m.CName
+	k.Realm = m.Realm
+	k.SName = m.SName
+	k.From = m.From
+	k.Till = m.Till
+	k.RTime = m.RTime
+	k.Nonce = m.Nonce
+	k.EType = m.EType
+	k.Addresses = m.Addresses
+	k.EncAuthData = m.EncAuthData
+	if len(m.AdditionalTickets.Bytes) > 0 {
+		k.AdditionalTickets, err = types.UnmarshalSequenceTickets(m.AdditionalTickets)
 		if err != nil {
-			return k, fmt.Errorf("Error unmarshalling additional tickets: %v", err)
+			return fmt.Errorf("Error unmarshalling additional tickets: %v", err)
 		}
 	}
-	k.ReqBody = reqb
-	return
+	return nil
 }
