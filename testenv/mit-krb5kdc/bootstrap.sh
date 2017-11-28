4468133d942f3995d74ad8ca962349474d52b4d7
Jonathan Turner <jt@jtnet.co.uk>
2017-11-28 05:12:42 -0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    # This is a combination of 13 commits.
    # This is the 1st commit message:
    
    support for DNS srv records and retry features for other KDCs
    
    # This is the commit message #2:
    
    filter out unsupported etypes in krb5.conf
    
    # This is the commit message #3:
    
    env var for ip
    
    # This is the commit message #4:
    
    update entrypoint
    
    # This is the commit message #5:
    
    intg test env
    
    # This is the commit message #6:
    
    dns entries
    
    # This is the commit message #7:
    
    travis nameserver
    
    # This is the commit message #8:
    
    dns intg tests
    
    # This is the commit message #9:
    
    fix network infinite loop
    
    # This is the commit message #10:
    
    fix issue
    
    # This is the commit message #11:
    
    travis env var
    
    # This is the commit message #12:
    
    missing port
    
    # This is the commit message #13:
    
    rebase
    
    # This is the commit message #2:
    
    update entrypoint
    
    # This is the commit message #2:
    
    dns entries
    
    # This is the commit message #2:
    
    fix network infinite loop
    
    # This is the commit message #2:
    
    missing port

--- testenv/mit-krb5kdc/bootstrap.sh
+++ testenv/mit-krb5kdc/bootstrap.sh
@@ -26,13 +26,14 @@ EOF
 
 cp /vagrant/krb5.conf /etc/krb5.conf
 cp /vagrant/*.service /etc/systemd/system/
-systemctl enable krb5kdc krb5kdc-latest krb5kdc-older httpd
+systemctl enable krb5kdc krb5kdc-latest krb5kdc-older httpd dns
 
 
 /usr/bin/docker pull jcmturner/gokrb5:http
 /usr/bin/docker pull jcmturner/gokrb5:kdc-centos-default
 /usr/bin/docker pull jcmturner/gokrb5:kdc-older
 /usr/bin/docker pull jcmturner/gokrb5:kdc-latest
+/usr/bin/docker pull jcmturner/gokrb5:dns
 
 
 reboot
