72011f9c07799fc144f5904fd06cfd17cab418e8
Jonathan Turner <jt@jtnet.co.uk>
2017-12-25 21:01:51 +0000

    red dom testenv

--- testenv/mit-krb5kdc/bootstrap.sh
+++ testenv/mit-krb5kdc/bootstrap.sh
@@ -26,11 +26,12 @@ EOF
 
 cp /vagrant/krb5.conf /etc/krb5.conf
 cp /vagrant/*.service /etc/systemd/system/
-systemctl enable krb5kdc krb5kdc-latest krb5kdc-older httpd dns
+systemctl enable krb5kdc krb5kdc-resdom krb5kdc-latest krb5kdc-older httpd dns
 
 
 /usr/bin/docker pull jcmturner/gokrb5:http
 /usr/bin/docker pull jcmturner/gokrb5:kdc-centos-default
+/usr/bin/docker pull jcmturner/gokrb5:kdc-resdom
 /usr/bin/docker pull jcmturner/gokrb5:kdc-older
 /usr/bin/docker pull jcmturner/gokrb5:kdc-latest
 /usr/bin/docker pull jcmturner/gokrb5:dns
@@ -38,4 +39,4 @@ systemctl enable krb5kdc krb5kdc-latest krb5kdc-older httpd dns
 
 reboot
 
-#systemctl start docker krb5kdc krb5kdc-latest krb5kdc-older
+#systemctl start docker krb5kdc krb5kdc-res krb5kdc-latest krb5kdc-older
