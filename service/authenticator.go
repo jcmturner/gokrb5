86fd7105dd1d241be63fa33ba24ee3d9ad9a697e
Hakan Carlsson <39298129+hakan-carlsson@users.noreply.github.com>
2018-10-09 07:27:16 +0200

    Update authenticator.go

--- service/authenticator.go
+++ service/authenticator.go
@@ -79,10 +79,13 @@ func (c *Config) Authenticate(neg, addr string) (i goidentity.Identity, ok bool,
 		err = fmt.Errorf("SPNEGO negotiation token is not a NegTokenInit: %v", err)
 		return
 	}
-	krb5Found := false
-        for i := 0 ; i < len(spnego.NegTokenInit.MechTypes) ; i++ {
-            if spnego.NegTokenInit.MechTypes[i].Equal(gssapi.MechTypeOIDKRB5) { krb5Found = true }
-        }
+	var krb5Found bool
+	for _, t := range spnego.NegTokenInit.MechTypes {
+ 	   if t.Equal(gssapi.MechTypeOIDKRB5) {
+        	krb5Found = true
+        	break
+    	   }
+	}
         if !krb5Found {
                 err = errors.New("SPNEGO OID of MechToken is not of type KRB5")
                 return
