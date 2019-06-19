816e07fc64c07a9c8fb05861038f96d33210e4ee
Jonathan Turner <jt@jtnet.co.uk>
2019-06-19 10:04:45 +0100

    fix tests

--- gssapi/gssapi_test.go
+++ gssapi/gssapi_test.go
@@ -15,10 +15,11 @@ func TestOID(t *testing.T) {
 		{OIDMSLegacyKRB5, []int{1, 2, 840, 48018, 1, 2, 2}},
 		{OIDKRB5, []int{1, 2, 840, 113554, 1, 2, 2}},
 		{OIDSPNEGO, []int{1, 3, 6, 1, 5, 5, 2}},
+		{OIDGSSIAKerb, []int{1, 3, 6, 1, 5, 2, 5}},
 	}
 
 	for _, tst := range tests {
 		oid := asn1.ObjectIdentifier(tst.oid)
-		assert.True(t, oid.Equal(OID(tst.name)), "OID value not as expected for %s", tst.name)
+		assert.True(t, oid.Equal(OIDName(tst.name).OID()), "OID value not as expected for %s", tst.name)
 	}
 }
