da348f3d5471668699cbf8d61a40f19d1d3eaa72
Jake Scott <jake@jakethesnake.dev>
2021-03-23 12:07:34 -0400

    Add support for creation / marshaling / enccryption of AP-REP messages
    Fixes #432

--- v8/messages/APRep_test.go
+++ v8/messages/APRep_test.go
@@ -8,8 +8,46 @@ import (
 	"github.com/jcmturner/gokrb5/v8/iana"
 	"github.com/jcmturner/gokrb5/v8/iana/msgtype"
 	"github.com/jcmturner/gokrb5/v8/test/testdata"
+	"github.com/jcmturner/gokrb5/v8/types"
 	"github.com/stretchr/testify/assert"
 )
+
+// Sample data from MIT Kerberos v1.19.1
+
+// from src/tests/asn.1/ktest.h
+const (
+	SAMPLE_USEC       = 123456
+	SAMPLE_SEQ_NUMBER = 17
+	SAMPLE_NONCE      = 42
+	SAMPLE_FLAGS      = 0xFEDCBA98
+	SAMPLE_ERROR      = 0x3C
+)
+
+func ktest_make_sample_ap_rep_enc_part() *EncAPRepPart {
+	tm, _ := time.Parse(testdata.TEST_TIME_FORMAT, testdata.TEST_TIME)
+	return &EncAPRepPart{
+		CTime:          tm,
+		Cusec:          SAMPLE_USEC,
+		Subkey:         *ktest_make_sample_keyblock(),
+		SequenceNumber: SAMPLE_SEQ_NUMBER,
+	}
+}
+
+func ktest_make_sample_keyblock() *types.EncryptionKey {
+	kv := []byte("12345678")
+	return &types.EncryptionKey{
+		KeyType:  1,
+		KeyValue: kv,
+	}
+}
+
+func ktest_make_sample_enc_data() *types.EncryptedData {
+	return &types.EncryptedData{
+		EType:  0,
+		KVNO:   5,
+		Cipher: []byte("krbASN.1 test message"),
+	}
+}
 
 func TestUnmarshalAPRep(t *testing.T) {
 	t.Parallel()
@@ -66,4 +104,51 @@ func TestUnmarshalEncAPRepPart_optionalsNULL(t *testing.T) {
 
 	assert.Equal(t, tt, a.CTime, "CTime not as expected")
 	assert.Equal(t, 123456, a.Cusec, "Client microseconds not as expected")
+}
+
+// test with all fields populated
+func TestAPRepEncPartMarshall(t *testing.T) {
+	t.Parallel()
+
+	want, err := hex.DecodeString(testdata.MarshaledKRB5ap_rep_enc_part)
+	assert.Nil(t, err, "error not expected decoding test data")
+
+	encpart := ktest_make_sample_ap_rep_enc_part()
+
+	b, err := encpart.Marshal()
+	assert.Nil(t, err, "enc part marshal error not expected")
+	assert.Equal(t, want, b)
+}
+
+// test with the optionals not present
+func TestAPRepEncPartMarshall_optionalsNULL(t *testing.T) {
+	t.Parallel()
+
+	want, err := hex.DecodeString(testdata.MarshaledKRB5ap_rep_enc_partOptionalsNULL)
+	assert.Nil(t, err, "error not expected decoding test data")
+
+	encpart := ktest_make_sample_ap_rep_enc_part()
+	encpart.SequenceNumber = 0
+	encpart.Subkey = types.EncryptionKey{}
+
+	b, err := encpart.Marshal()
+	assert.Nil(t, err, "enc part marshal error not expected")
+	assert.Equal(t, want, b)
+}
+
+func TestAprepMarshal(t *testing.T) {
+	t.Parallel()
+
+	want, err := hex.DecodeString(testdata.MarshaledKRB5ap_rep)
+	assert.Nil(t, err, "error not expected decoding test data")
+
+	aprep := APRep{
+		PVNO:    iana.PVNO,
+		MsgType: msgtype.KRB_AP_REP,
+		EncPart: *ktest_make_sample_enc_data(),
+	}
+
+	b, err := aprep.Marshal()
+	assert.Nil(t, err, "enc part marshal error not expected")
+	assert.Equal(t, want, b)
 }
